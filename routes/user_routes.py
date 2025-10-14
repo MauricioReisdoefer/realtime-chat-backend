01a3fb7e150ee2717fff5068d53e63640c10d057
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-10-13 22:31:55 -0300

    feat: adding user routes

---
+++ routes/user_routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, jsonify
+from controller.user_controller import create_user as create_user_func, get_all, filter_by_id
+
+user_bp = Blueprint("user", url_prefix="/user")
+
+@user_bp.route('/create', methods=['POST'])
+def create_user():
+    return create_user_func()
+
+@user_bp.route('/all', methods=['GET'])
+def get_users():
+    users = get_all()
+    data = [{"id": u.id, "username": u.username} for u in users]
+    return jsonify(data), 200
+
+@user_bp.route('/filter/<string:username>', methods=['GET'])
+def get_by_name(username):
+    users = filter_by_id(username)
+    data = [{"id": u.id, "username": u.username} for u in users]
+    return jsonify(data), 200
